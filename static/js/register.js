a6fa2831ac38bb000dc93775f4eb0e49946119d7
prashanth <prashanthjbabu@gmail.com>
2013-06-27 20:04:11 +0530

    working on registration

--- static/js/register.js
+++ static/js/register.js
@@ -1,14 +1,23 @@
 function validateregister()
 {
+	$("#register-error-content").text("");
 	var name=document.getElementById("name").value;
 	var email=document.getElementById("email").value;
 	var password=document.getElementById("password").value;
 	var cpassword=document.getElementById("cpassword").value;
+
+	var emailfilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 
 	if(name==null || name=="")
 	{
 		$("#register-error-content").text("Please enter a valid name");
 		$("#name").focus();
 		return;
-	}	
+	}
+
+    if (!emailfilter.test(email)) {
+	$("#register-error-content").text("Please enter a valid email");
+    $("#email").focus();
+    return;
+ }	
 }
