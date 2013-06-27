04896cb58ecd1de3032d413e9b32828725d2892f
prashanth <prashanthjbabu@gmail.com>
2013-06-27 19:36:04 +0530

    working on registration

---
+++ static/js/register.js
@@ -0,0 +1,7 @@
+function register()
+{
+	var name=document.getElementById("name").value;
+	var email=document.getElementById("email").value;
+	var password=document.getElementById("password").value;
+	var cpassword=document.getElementById("cpassword").value;
+}
