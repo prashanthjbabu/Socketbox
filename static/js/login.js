f065f7b8d60f9d4bbf470f798dd3d820c872d6ea
prashanth <prashanthjbabu@gmail.com>
2013-06-30 02:25:38 +0530

    javascript login part

--- static/js/login.js
+++ static/js/login.js
@@ -5,7 +5,7 @@ function userlogin()
 	var emailfilter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
  	var passfilter=  /^[A-Za-z]\w{7,14}$/;
 
- 	if (!emailfilter.test(email) || !passfilter.test(password)) 
+ 	if (!emailfilter.test(navemail) || !passfilter.test(navpass)) 
     {
     	window.location.href='/socketbox/login/';
 	}
