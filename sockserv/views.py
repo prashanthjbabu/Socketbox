eba79968c6280a678da2d20c45420cf8a319a727
prashanth <prashanthjbabu@gmail.com>
2013-06-30 03:39:42 +0530

    logout part

--- sockserv/views.py
+++ sockserv/views.py
@@ -5,7 +5,7 @@ from django.utils import simplejson
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
+#@csrf_exempt
 def index(request):
 	return render_to_response('index.html', context_instance=RequestContext(request))
 
