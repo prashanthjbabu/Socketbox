f194b5dfd1dba6a781f3082b605aa7dd33869519
prashanth <prashanthjbabu@gmail.com>
2013-07-02 19:22:42 +0530

    encrypting passwords at client end instead of server end

--- static/js/encryptpass.js
+++ static/js/encryptpass.js
@@ -3,4 +3,5 @@ function encryptnavpass()
 
 	document.getElementById("navpass").value=$.md5(document.getElementById("navpass").value);
 	console.log(document.getElementById("navpass").value);
+	alert("HI");
 }
