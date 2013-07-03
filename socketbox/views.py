1bcc94590848d12b91562bd75d9f2b25a8bc90b4
prashanth <prashanthjbabu@gmail.com>
2013-07-03 23:11:50 +0530

    working on lastmessagesent

--- socketbox/views.py
+++ socketbox/views.py
@@ -405,7 +405,7 @@ def dashboard(request) :
 			myappmsgcount=stats.objects.filter(appid=app.id).count()
 			myapplastupdate=stats.objects.filter(appid=app.id).order_by('-time')[:1]
 			apptimelog = {}
-			apptimelog['time']=myapplastupdate
+			apptimelog['time']=myapplastupdate.time
 			timelog.append(apptimelog)
 			totalmsgcount+=myappmsgcount
 			appdata = {
