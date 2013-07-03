db36f847e50bf8cd6159825a56d56382e9028416
prashanth <prashanthjbabu@gmail.com>
2013-07-03 23:20:01 +0530

    working on lastmessagesent

--- socketbox/views.py
+++ socketbox/views.py
@@ -427,7 +427,7 @@ def dashboard(request) :
 		applogs=stats.objects.filter()
 		timelog=sorted(timelog,key=lambda x:x['time'],reverse=True)
 		lastupdatetime=timelog[0]['time']
-		print "last update time is "+lastupdatetime
+		print "last update time is "+str(lastupdatetime)
 		return render_to_response('dashboard.html',{'lastupdatetime' : lastupdatetime , 'appscount' : appscount, 'myapps' : myapps,'applog' : applog ,'activeappstatus' : popularappdata['status'],'activeapp' : popularappdata['app'],'activeappcount' : popularappdata['count'], 'msgcount' : totalmsgcount }, context_instance=RequestContext(request))	
 	else :
 		#session does not exist for user redirect to login screen
