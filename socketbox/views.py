787a28725c0157ccf839b8218897625e0f7986cd
prashanth <prashanthjbabu@gmail.com>
2013-07-04 18:17:59 +0530

    playing with timezone

--- socketbox/views.py
+++ socketbox/views.py
@@ -436,7 +436,7 @@ def dashboard(request) :
 		lastupdatetime=timelog[0]['time']
 		currtime=datetime.datetime.now()
 		print "last update time is "+str(lastupdatetime)
-		return render_to_response('dashboard.html',{'currtime' : currtime 'lastupdatetime' : lastupdatetime , 'appscount' : appscount, 'myapps' : myapps,'applog' : applog ,'activeappstatus' : popularappdata['status'],'activeapp' : popularappdata['app'],'activeappcount' : popularappdata['count'], 'msgcount' : totalmsgcount }, context_instance=RequestContext(request))	
+		return render_to_response('dashboard.html',{'currtime' : currtime , 'lastupdatetime' : lastupdatetime , 'appscount' : appscount, 'myapps' : myapps,'applog' : applog ,'activeappstatus' : popularappdata['status'],'activeapp' : popularappdata['app'],'activeappcount' : popularappdata['count'], 'msgcount' : totalmsgcount }, context_instance=RequestContext(request))	
 	else :
 		#session does not exist for user redirect to login screen
 		return render_to_response('login.html', context_instance=RequestContext(request))	
