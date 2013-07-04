1c92f6433fbe4bfac4e31aee0de26e1d99338944
prashanth <prashanthjbabu@gmail.com>
2013-07-04 19:17:41 +0530

    playing with timezone

--- sockserv/settings.py
+++ sockserv/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = []
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
 #TIME_ZONE = 'America/Chicago'
-#TIME_ZONE = ''
+TIME_ZONE = '...'
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
