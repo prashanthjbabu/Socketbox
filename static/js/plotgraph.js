2991661009aadbacfb5c515a2b8175c74a332242
prashanth <prashanthjbabu@gmail.com>
2013-07-05 09:35:18 +0530

    trying another le chart

--- static/js/plotgraph.js
+++ static/js/plotgraph.js
@@ -2,7 +2,7 @@
         //var data = [[0, 11],[1, 15],[2, 25],[3, 24],[4, 13],[5, 18]];
         var data= []
         var ticks = []
-        var dataset = [{ label: "Socketbox App Statistics", data: data, color: "#5482FF" }];        
+        var dataset = [{ label: "", data: data, color: "#5482FF" }];        
         console.log("PLOTGRAPHLOADED");
         var options = {
             series: {
