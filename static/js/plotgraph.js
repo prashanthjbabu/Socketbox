229f631eb364352e1262c40383419dfc3bac1088
prashanth <prashanthjbabu@gmail.com>
2013-07-05 09:30:13 +0530

    trying another le chart

--- static/js/plotgraph.js
+++ static/js/plotgraph.js
@@ -1,6 +1,8 @@
         //******* 2012 Average Temperature - BAR CHART
         //var data = [[0, 11],[1, 15],[2, 25],[3, 24],[4, 13],[5, 18]];
+        var data= []
         var ticks = []
+        var dataset = [{ label: "Socketbox App Statistics", data: data, color: "#5482FF" }];        
         console.log("PLOTGRAPHLOADED");
         var options = {
             series: {
