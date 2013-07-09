53ec8a3cb9be280ca3c871480c97b9db46d33ee9
prashanth <prashanthjbabu@gmail.com>
2013-07-09 08:49:52 +0530

    updated SMTP settings

--- sockserv/settings.py
+++ sockserv/settings.py
@@ -24,7 +24,8 @@ DATABASES = {
 EMAIL_HOST='smtp.socketbox.in'
 EMAIL_HOST_USER='no-reply@socketbox.in'
 EMAIL_HOST_PASSWORD='Harshit123'
-EMAIL_PORT=587;
+#EMAIL_PORT=587;
+EMAIL_PORT=25;
 EMAIL_USE_TLS=True
 
 
